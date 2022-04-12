11ba794ea1de0cafeb3700790b6aa5d44c64f274
Zuhridin_Bakhriddinov <zukicgleo@gmail.com>
2022-04-12 09:46:05 +0500

    security added

--- src/main/java/uz/pdp/olchauzcloneapp/repository/RoleRepository.java
+++ src/main/java/uz/pdp/olchauzcloneapp/repository/RoleRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import uz.pdp.olchauzcloneapp.entity.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+
 }
