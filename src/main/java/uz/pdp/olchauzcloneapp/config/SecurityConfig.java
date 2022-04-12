92b9269ec57b2672cd09d3b723b7ddbba65c55c9
Zuhridin_Bakhriddinov <zukicgleo@gmail.com>
2022-04-12 09:35:26 +0500

    security added

---
+++ src/main/java/uz/pdp/olchauzcloneapp/config/SecurityConfig.java
@@ -0,0 +1,70 @@
+package uz.pdp.olchauzcloneapp.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import uz.pdp.olchauzcloneapp.security.JwtFilter;
+import uz.pdp.olchauzcloneapp.service.AuthService;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    AuthService authService;
+    @Autowired
+    JwtFilter jwtFilter;
+    @Autowired
+    AuthService userService;
+
+
+
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(authService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+     http.
+                csrf().disable()
+                .httpBasic().disable()
+                .authorizeRequests()
+                .antMatchers("/auth/**").permitAll()
+                .anyRequest()
+              .permitAll();
+//                .authenticated();
+
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+
+
+
+    }
+
+
+}
---
+++ src/main/java/uz/pdp/olchauzcloneapp/service/AuthService.java
@@ -0,0 +1,98 @@
+package uz.pdp.olchauzcloneapp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import uz.pdp.olchauzcloneapp.entity.User;
+import uz.pdp.olchauzcloneapp.entity.template.ApiResponse;
+import uz.pdp.olchauzcloneapp.payload.LoginDto;
+import uz.pdp.olchauzcloneapp.payload.RegisterDto;
+import uz.pdp.olchauzcloneapp.repository.RoleRepository;
+import uz.pdp.olchauzcloneapp.repository.UserRepository;
+import uz.pdp.olchauzcloneapp.security.JwtProvider;
+
+
+import java.util.Collections;
+
+// Zuhridin Bakhriddinov 4/5/2022 11:45 AM
+@Service
+public class AuthService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    JwtProvider jwtProvider;
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    public HttpEntity registerUser(RegisterDto registerDto) {
+
+        if (userRepository.existsByEmail(registerDto.getEmail())) {
+            return new ResponseEntity<>(new ApiResponse("Wrong", false, false), HttpStatus.ALREADY_REPORTED);
+        }
+        User user = new User(registerDto.getFullName(), registerDto.getEmail(),
+                passwordEncoder.encode(registerDto.getPassword()), Collections.singleton(roleRepository.findByName("ROLE_USER"))
+        );
+
+
+        userRepository.save(user);
+
+
+        return new ResponseEntity<>(new ApiResponse("Successfully Registered.", true, true), HttpStatus.OK);
+
+
+    }
+
+
+
+
+
+    public HttpEntity login(LoginDto loginDto) {
+
+        try {
+            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                    loginDto.getEmail(), loginDto.getPassword()
+            ));
+
+            User principal = (User) authentication.getPrincipal();
+            String generatedToken = jwtProvider.generateToken(principal.getEmail(), true);
+            return new ResponseEntity<>(new ApiResponse("Token", true, generatedToken), HttpStatus.OK);
+
+
+        } catch (BadCredentialsException e) {
+            return new ResponseEntity<>(new ApiResponse("Email or password not found", false, false), HttpStatus.NOT_FOUND);
+
+        }
+    }
+
+
+
+}
+
+
+
+
+
+
